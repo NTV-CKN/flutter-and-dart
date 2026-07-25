242365a148ef9ee2e04a958578053a0b7e83951a
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-07-25 17:09:16 +0700

    Ghi đè các callback của stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
-
-const colors = [Colors.deepPurple, Colors.indigo];
+import 'package:second_app/ui_update_demo.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-        body: Text('Hello world!')
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const UIUpdatesDemo(),
       ),
-    ),
-  );
+    );
+  }
 }
---
+++ lib/ui_update_demo.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class UIUpdatesDemo extends StatefulWidget {
+  const UIUpdatesDemo({super.key});
+
+  @override
+  StatefulElement createElement() {
+    print('UIUpdatesDemo createElement called');
+    return super.createElement();
+  }
+
+  @override
+  // ignore: no_logic_in_create_state
+  State<UIUpdatesDemo> createState() {
+    print('UIUpdatesDemo createState called');
+    return _UIUpdatesDemo();
+  }
+}
+
+class _UIUpdatesDemo extends State<UIUpdatesDemo> {
+  var _isUnderstood = false;
+
+  @override
+  void initState() {
+    print('UIUpdatesDemo initState called');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('UIUpdatesDemo didChangeDependencies called');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant UIUpdatesDemo oldWidget) {
+    print('UIUpdatesDemo didUpdateWidget called');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    print('UIUpdatesDemo setState called');
+    super.setState(fn);
+  }
+
+  @override
+  void deactivate() {
+    print('UIUpdatesDemo deactivate called');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('UIUpdatesDemo dispose called');
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('UIUpdatesDemo BUILD called');
+
+    //register dependency MediaQuery
+    final widthSpace = MediaQuery.of(context).viewInsets.bottom;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(),
+            const Text(
+              'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Do you understand how Flutter updates UIs?',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isUnderstood = false;
+                    });
+                  },
+                  child: const Text('No'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isUnderstood = true;
+                    });
+                  },
+                  child: const Text('Yes'),
+                ),
+              ],
+            ),
+            if (_isUnderstood) const Text('Awesome!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
