553296fccee9a5786f89c4c13f71505a7d7daf58
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-07-26 12:16:30 +0700

    khởi tạo dự án

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-const colors = [Colors.deepPurple, Colors.indigo];
+final kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 161, 123, 238),
+);
+
+final theme = ThemeData(
+  colorScheme: kColorScheme,
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Text('Hello world!')
-      ),
-    ),
+    Main(),
   );
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: Scaffold(
+        body: Text('Hello world!'),
+      ),
+    );
+  }
 }
